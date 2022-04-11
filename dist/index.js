c42aeec21632ff4973bb371cdd13391d49d49b96
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-11 23:08:15 +0700

    add: basic types, array and tuples

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,21 @@
 "use strict";
+//Basic types
 let id = 5;
-console.log('ID:', id);
+// console.log('ID:', id);
+let company = 'Mappa';
+let isPublidhed = true;
+let x = 'Hello';
+let age;
+age = 20;
+let ids = [1, 2, 3, 4, 5,];
+// ids.push(2)
+let arr = [1, true, 'Hello'];
+//Tuple
+let person = [1, 'hello', true];
+//Tuple array
+let employee;
+employee = [
+    [1, "eren"],
+    [2, "mikasa"],
+    [3, "armin"],
+];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+//Basic types
 let id: number = 5;
+// console.log('ID:', id);
+let company: string = 'Mappa'
+let isPublidhed: boolean = true
+let x: any = 'Hello'
+let age: number 
+age = 20
 
-console.log('ID:', id);
+let ids: number[] = [1,2,3,4,5,]
+// ids.push(2)
+let arr: any[] = [1, true, 'Hello']
+
+//Tuple
+let person: [number, string, boolean] = [1, 'hello', true]
+//Tuple array
+let employee:  [number, string][]
+employee = [
+  [1, "eren"],
+  [2, "mikasa"],
+  [3, "armin"],
+];
